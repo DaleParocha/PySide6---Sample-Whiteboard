948ac38846c7b301441c1a1e5cf2aa4acf04f540
DaleParocha <workingamap@gmail.com>
2026-08-07 22:13:03 +0800

    mouseMoveEvent update check last_pos cursor

--- workspace/workspace.py
+++ workspace/workspace.py
@@ -16,18 +16,23 @@ class Workspace(QWidget):
         self.last_pos = event.position()
 
     def mouseMoveEvent(self, event):
-        current_pos = event.position()        
-        painter = QPainter(self.canvas)
-        painter.setPen(QPen(QColor("black"), 3))
-        painter.drawLine(self.last_pos, current_pos)
-        painter.end()
+        if self.last_pos is not None:
+            current_pos = event.position()        
+            painter = QPainter(self.canvas)
+            painter.setPen(QPen(QColor("black"), 3))
+            painter.drawLine(self.last_pos, current_pos)
+            painter.end()
 
-        self.last_pos = current_pos
-        self.update()
+            self.last_pos = current_pos
 
+            # update every Move event if last_pos != None
+            self.update()
+
+# on release of mouse press return mouse last_pos to None
     def mouseReleaseEvent(self, event):
         self.last_pos = None
 
+# start painting
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(0, 0, self.canvas)
